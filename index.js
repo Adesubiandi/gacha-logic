3f745869279da694ad975b4d95b53f58a8e564de
Adesubiandi <96976160+Adesubiandi@users.noreply.github.com>
2022-01-03 23:40:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 // Selengkapnya di /resource/
 console.log(require('./resources/print-out.js'));
 event_rate.  *=100;
-sr_rste.  *=1.5;
+sr_rate.  *=1.5;
 Ssr_rate.   *=8.5;
 R_rste.  *=0.1;
 (one_or_ten ===true)  {
