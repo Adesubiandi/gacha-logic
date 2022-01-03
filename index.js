54d57e8d1e613f1dff7152c9272d31500831a132
Adesubiandi <96976160+Adesubiandi@users.noreply.github.com>
2022-01-04 00:31:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,13 @@ console.log(require('./resources/print-out.js'));
 event_rate.  *=100;
 sr_rate.  *=1.5;
 Ssr_rate.   *=8.5;
-R_rate.  *=0.1;
+r_rate.  *=0.1;
 (one_or_ten ===true)  {
+
+
+//**
+@param {A.T.S<event_rate>}
+@param {number}kecil untuk mendapat kartu sr_
+@param {number} kecil untuk mendapat kartu r_
+@param {number}besar untuk mendapat kartu ssr_
+@param {number} besar untuk mendapat kartu event
